4a7b79cc190b9123bbf80d5a28f185c40d91d5bb
Jeann Lucas <jeann.lucas@outlook.com>
2024-05-02 14:10:53 -0300

    feat: creating columns in the courses_tags table through migration

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,6 +3,8 @@ import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1714652174496 } from 'src/migrations/1714652174496-CreateCoursesTable';
 import { CreateTagsTable1714658182098 } from 'src/migrations/1714658182098-CreateTagsTable';
 import { CreateCoursesTagsTable1714660810750 } from 'src/migrations/1714660810750-CreateCoursesTagsTable';
+import { AddCoursesIdToCoursesTagsTable1714667469864 } from 'src/migrations/1714667469864-AddCoursesIdToCoursesTagsTable';
+import { AddTagsIdToCoursesTagsTable1714668403192 } from 'src/migrations/1714668403192-AddTagsIdToCoursesTagsTable';
 
 export const database = new DataSource({
   ...dataSourceOptions,
@@ -11,5 +13,7 @@ export const database = new DataSource({
     CreateCoursesTable1714652174496,
     CreateTagsTable1714658182098,
     CreateCoursesTagsTable1714660810750,
+    AddCoursesIdToCoursesTagsTable1714667469864,
+    AddTagsIdToCoursesTagsTable1714668403192,
   ],
 });
