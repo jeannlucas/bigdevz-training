97425013ca60f34e15b7d904c540ca00dbf95567
Jeann Lucas <jeann.lucas@outlook.com>
2024-05-02 13:28:35 -0300

    feat: creating the courses_tags table through migration

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,14 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1714652174496 } from 'src/migrations/1714652174496-CreateCoursesTable';
 import { CreateTagsTable1714658182098 } from 'src/migrations/1714658182098-CreateTagsTable';
+import { CreateCoursesTagsTable1714660810750 } from 'src/migrations/1714660810750-CreateCoursesTagsTable';
 
 export const database = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1714652174496, CreateTagsTable1714658182098],
+  migrations: [
+    CreateCoursesTable1714652174496,
+    CreateTagsTable1714658182098,
+    CreateCoursesTagsTable1714660810750,
+  ],
 });
