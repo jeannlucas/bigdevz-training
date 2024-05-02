d5e8b56545c78d186118ba19cf718cf1afb2b70d
Jeann Lucas <jeann.lucas@outlook.com>
2024-05-02 11:37:57 -0300

    feat: refactoring of the service and course and tag controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -23,7 +23,7 @@ export class CoursesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
+  findOne(@Param('id') id: string) {
     return this.courseService.findOne(id);
   }
 
@@ -33,13 +33,13 @@ export class CoursesController {
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() updateCourseDTO: updateCourseDTO) {
+  update(@Param('id') id: string, @Body() updateCourseDTO: updateCourseDTO) {
     return this.courseService.update(id, updateCourseDTO);
   }
 
   @HttpCode(204)
   @Delete(':id')
-  remove(@Param('id') id: number) {
+  remove(@Param('id') id: string) {
     return this.courseService.remove(id);
   }
 }
